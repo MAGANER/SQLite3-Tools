0649f32c701bb1bd914022e12d6ea4dc59505687
MAGANER <maganer.krik@mail.ru>
2021-06-17 17:25:40 +0300

    fixed final and the most cruel bug, victory!

--- DataBase.cpp
+++ DataBase.cpp
@@ -41,18 +41,18 @@ vector<chunk> DataBase::run_get_request(const string& request)
 	if (error_message != nullptr)
 		this->error_message = error_message;
 
-
-	data->erase(--data->end());//last element is junk
 	return *data;
 }
 
 int DataBase::get_request_callback(void* data, int argc, char** argv, char** azColName)
 {
 	map<string, SQLtype*> extracted_data;
+
 	for (int i = 0; i < argc; ++i)
 	{
 		function<char(char)> to_lower = tolower;
 		string name = Functools::map(string(azColName[i]), to_lower);
+		
 		if (name == "id")
 		{
 			int id_val = argv[i] ? atoi(argv[i]) : 0;
@@ -88,10 +88,9 @@ int DataBase::get_request_callback(void* data, int argc, char** argv, char** azC
 				//if there is nothing just skip it
 			}
 		}
-
-		static_cast<vector<chunk>*>(data)->push_back(extracted_data);
-		extracted_data.clear();
+		
 	}
+	static_cast<vector<chunk>*>(data)->push_back(extracted_data);
 	
 	return 0;
 }
@@ -110,9 +109,10 @@ vector<chunk> DataBase::run_get_request(const string& request, function<bool(str
 
 	if (error_message != nullptr)
 		this->error_message = error_message;
-	data->erase(--data->end()); //last element is junk
 
 	vector<chunk>* result = new vector<chunk>();
+
+
 	for (auto _chunk : *data)
 	{
 		chunk curr;
